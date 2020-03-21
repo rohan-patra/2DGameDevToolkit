b2073a90dcb6c966002f5f9af3c136adbda67eed
Rohan Patra <rohanpatra2003@gmail.com>
2020-03-20 18:53:29 -0700

    Add files via upload

---
+++ rohans2dtlkit/__init__.py
@@ -0,0 +1,10 @@
+from .utils import floor, line, path, square, vector
+
+__all__ = ['floor', 'line', 'path', 'square', 'vector']
+
+__title__ = 'rohans2dtlkit'
+__version__ = '2.3.2'
+__build__ = 0x020302
+__author__ = 'Grant Jenks'
+__license__ = 'Apache 2.0'
+__copyright__ = '2017-2020, Grant Jenks'
